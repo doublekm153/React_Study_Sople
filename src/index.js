705a63db5530d1242b54653b836eb21ea56fd432
doublekm153 <doublekm153@gmail.com>
2023-09-14 10:36:34 +0900

    chap_03_v2

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-//import Library from "./chap03/Library";
+import Library from "./chap03/Library";
 //import Clock from "./chap_04/Clock";
 //import CommentList from "./chap_05/CommentList";
 //import NotificationList from "./chap_06/NotificationList";
 //import Accommodate from "./chap_07/Accommodate";
 //import UserStatus from "./chap_07/UserStatus";
 //const root = ReactDOM.createRoot(document.getElementById("root"));
-import ConfirmButton from "./chap_08/ConfirmButton";
+//import ConfirmButton from "./chap_08/ConfirmButton";
 
 ReactDOM.render(
     <React.StrictMode>
-        <ConfirmButton />
+        <Library />
     </React.StrictMode>,
     document.getElementById("root")
 );
